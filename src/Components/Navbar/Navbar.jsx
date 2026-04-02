3eb332cc1b557bbab661c95f847a8d5de8fa0267
Dulal Ahmed <ahmeddulal4211@gmail.com>
2026-04-02 18:24:57 +0600

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import Logo from '../../assets/fc-barcelona-logo-png-transparent.png'
 import { User2 } from 'lucide-react';
 const Navbar = ({coin}) => {
   return (
-    <div classname="fixed left-0 right-0 top-0">
-     <div className="navbar bg-base-100 shadow-sm p-5 lg:p-10 border-b border-red-700 pb-2   ">
+    <div>
+     <div className="navbar bg-base-100 shadow-sm p-5 lg:p-10 border-b border-red-700 pb-2  fixed left-0 right-0 top-0"> ">
   <div className="navbar-start">
     <div className="dropdown mr-3 md:mr-0">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
